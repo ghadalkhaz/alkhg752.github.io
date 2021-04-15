a00aeb37ce969fa94bb6beed2e4efbbea0756270
ghadalkhaz <alkhg752@newschool.edu>
2021-04-15 17:55:45 -0400

    go

---
+++ elastic-collections/landing-page/qr-codes/script.js
@@ -0,0 +1,224 @@
+function display_random_image() {
+     var theImages = [{
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FIMG_9703.JPG?v=1617910481575"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-13%20at%208.04.41%20PM.png?v=1618358712740"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-13%20at%208.04.58%20PM.png?v=1618358732900"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.18.17%20AM.png?v=1618510843781"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.18.10%20AM.png?v=1618510872103"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.18.04%20AM.png?v=1618510890993"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.17.59%20AM.png?v=1618510914379"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.17.53%20AM.png?v=1618510933824"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.17.46%20AM.png?v=1618510953808"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.17.38%20AM.png?v=1618510975113"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.17.33%20AM.png?v=1618511063568"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.17.28%20AM.png?v=1618511084603"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.17.23%20AM.png?v=1618511150744"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.17.16%20AM.png?v=1618511235056"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.17.09%20AM.png?v=1618511251205"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.17.01%20AM.png?v=1618511392322"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.16.55%20AM.png?v=1618511410106"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.16.44%20AM.png?v=1618511437307"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.16.38%20AM.png?v=1618511453625"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.16.33%20AM.png?v=1618511475718"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.16.26%20AM.png?v=1618511496875"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.16.21%20AM.png?v=1618511522579"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.16.14%20AM.png?v=1618511537764"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.16.06%20AM.png?v=1618511554350"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.15.56%20AM.png?v=1618511578316"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.15.50%20AM.png?v=1618511598828"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.15.43%20AM.png?v=1618511615282"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.15.34%20AM.png?v=1618511631693"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.15.26%20AM.png?v=1618511648136"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.15.21%20AM.png?v=1618511773844"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.15.15%20AM.png?v=1618511833152"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.15.09%20AM.png?v=1618511867718"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.15.00%20AM.png?v=1618511885754"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.14.54%20AM.png?v=1618511901068"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.14.47%20AM.png?v=1618511966884"
+    },{
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.14.40%20AM.png?v=1618511988967"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.14.33%20AM.png?v=1618512006564"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.14.07%20AM.png?v=1618512025148"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%2012.14.00%20AM.png?v=1618512048495"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.28.13%20AM.png?v=1618512068054"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.28.05%20AM.png?v=1618512111770"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.27.59%20AM.png?v=1618512129879"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.27.51%20AM.png?v=1618512149931"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.27.41%20AM.png?v=1618512172564"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.27.31%20AM.png?v=1618512190616"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.27.22%20AM.png?v=1618512209956"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.27.13%20AM.png?v=1618512239263"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.27.07%20AM.png?v=1618512255870"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.26.56%20AM.png?v=1618512277459"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.26.49%20AM.png?v=1618512295160"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.26.43%20AM.png?v=1618512345979"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.26.37%20AM.png?v=1618512361602"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.26.27%20AM.png?v=1618512414594"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.26.20%20AM.png?v=1618512432699"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.26.10%20AM.png?v=1618512449702"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.25.03%20AM.png?v=1618512470837"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.24.40%20AM.png?v=1618512531373"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.24.34%20AM.png?v=1618512550686"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.23.48%20AM.png?v=1618512577624"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.23.40%20AM.png?v=1618512594438"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.23.33%20AM.png?v=1618512607920"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.23.33%20AM.png?v=1618512607920"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.23.27%20AM.png?v=1618512640874"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.23.22%20AM.png?v=1618512667082"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.23.12%20AM.png?v=1618512687312"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.23.01%20AM.png?v=1618512707644"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.22.45%20AM.png?v=1618512722255"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.24.11%20AM.png?v=1618512743768"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.23.58%20AM.png?v=1618512764920"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.24.04%20AM.png?v=1618512793320"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.21.58%20AM.png?v=1618512840333"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.22.04%20AM.png?v=1618512855602"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.21.52%20AM.png?v=1618512875390"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.21.46%20AM.png?v=1618512889149"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.21.39%20AM.png?v=1618512907598"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.21.32%20AM.png?v=1618512923171"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.21.12%20AM.png?v=1618512945952"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.21.05%20AM.png?v=1618512967746"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.20.57%20AM.png?v=1618512984728"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.20.50%20AM.png?v=1618513001297"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.20.46%20AM.png?v=1618513017235"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.20.39%20AM.png?v=1618513033664"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.20.33%20AM.png?v=1618513046677"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.20.23%20AM.png?v=1618513060693"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.16.41%20AM.png?v=1618513084160"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.16.35%20AM.png?v=1618513117918"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.16.28%20AM.png?v=1618513133470"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.16.28%20AM.png?v=1618513133470"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.16.02%20AM.png?v=1618513181274"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.15.51%20AM.png?v=1618513216930"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-13%20at%208.04.58%20PM.png?v=1618513232588"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-13%20at%208.04.41%20PM.png?v=1618513245229"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.24.27%20AM.png?v=1618513268814"
+    }, {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.24.17%20AM.png?v=1618513284147"
+    },  {
+        src: "https://cdn.glitch.com/319fa752-a039-442d-ab33-8ed73faf8d53%2FScreen%20Shot%202021-04-15%20at%201.24.04%20AM.png?v=1618513302137"
+    }, ];
+
+    var preBuffer = [];
+    for (var i = 0, j = theImages.length; i < j; i++) {
+        preBuffer[i] = new Image();
+        preBuffer[i].src = theImages[i].src;
+        //preBuffer[i].width = theImages[i].width;
+        //preBuffer[i].height = theImages[i].height;
+    }
+
+  // create random image number
+  function getRandomInt(min,max) {
+    var imn = Math.floor(Math.random() * (max - min + 1)) + min;
+    return preBuffer[imn];
+  }
+
+  // 0 is first image,   preBuffer.length - 1) is  last image
+
+  var newImage = getRandomInt(0, preBuffer.length - 1);
+
+  // remove the previous images
+  var images = document.getElementsByTagName('img');
+  var l = images.length;
+  for (var p = 0; p < l; p++) {
+    images[0].parentNode.removeChild(images[0]);
+  }
+
+  // display the image
+  console.log(newImage);
+  document.body.appendChild(newImage);
+}
+
+display_random_image();
