6969f48744d2978eaae6ce7f118dd2a870be3374
ghadalkhaz <alkhg752@newschool.edu>
2021-05-06 20:53:11 -0400

    go

---
+++ input-output/love-on-tour/press-any-key/script.js
@@ -0,0 +1,313 @@
+//to play on the key A do this(using Jquery):
+const audio = document.getElementById('audio');
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'a') {
+  	if (!audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+    } else {
+    	audio.play();
+    }
+  }
+});
+
+const jokes = document.getElementById('jokes');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'j') {
+  	if (!jokes.paused) {
+      jokes.pause();
+      jokes.currentTime = 0;
+    } else {
+    	jokes.play();
+    }
+  }
+});
+
+const crying = document.getElementById('crying');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'y') {
+  	if (!crying.paused) {
+      crying.pause();
+      crying.currentTime = 0;
+    } else {
+    	crying.play();
+    }
+  }
+});
+
+const shh = document.getElementById('shh');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 's') {
+  	if (!shh.paused) {
+      shh.pause();
+      shh.currentTime = 0;
+    } else {
+    	shh.play();
+    }
+  }
+});
+
+const love = document.getElementById('love');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'i') {
+  	if (!love.paused) {
+      love.pause();
+      love.currentTime = 0;
+    } else {
+    	love.play();
+    }
+  }
+});
+
+const laugh = document.getElementById('laugh');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'l') {
+  	if (!laugh.paused) {
+      laugh.pause();
+      laugh.currentTime = 0;
+    } else {
+    	laugh.play();
+    }
+  }
+});
+
+const wind = document.getElementById('wind');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'w') {
+  	if (!wind.paused) {
+      wind.pause();
+      wind.currentTime = 0;
+    } else {
+    	wind.play();
+    }
+  }
+});
+
+const rain = document.getElementById('rain');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'r') {
+  	if (!rain.paused) {
+      rain.pause();
+      rain.currentTime = 0;
+    } else {
+    	rain.play();
+    }
+  }
+});
+
+const cheer = document.getElementById('cheer');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'q') {
+  	if (!cheer.paused) {
+      cheer.pause();
+      cheer.currentTime = 0;
+    } else {
+    	cheer.play();
+    }
+  }
+});
+
+const scream = document.getElementById('scream');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'e') {
+  	if (!scream.paused) {
+      scream.pause();
+      scream.currentTime = 0;
+    } else {
+    	scream.play();
+    }
+  }
+});
+
+const guy = document.getElementById('guy');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'g') {
+  	if (!guy.paused) {
+      guy.pause();
+      guy.currentTime = 0;
+    } else {
+    	guy.play();
+    }
+  }
+});
+
+const whistle = document.getElementById('whistle');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'f') {
+  	if (!whistle.paused) {
+      whistle.pause();
+      whistle.currentTime = 0;
+    } else {
+    	whistle.play();
+    }
+  }
+});
+
+const talking = document.getElementById('talking');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 't') {
+  	if (!talking.paused) {
+      talking.pause();
+      talking.currentTime = 0;
+    } else {
+    	talking.play();
+    }
+  }
+});
+
+const people = document.getElementById('people');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'p') {
+  	if (!people.paused) {
+      people.pause();
+      people.currentTime = 0;
+    } else {
+    	people.play();
+    }
+  }
+});
+
+const ambulance = document.getElementById('ambulance');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'b') {
+  	if (!ambulance.paused) {
+      ambulance.pause();
+      ambulance.currentTime = 0;
+    } else {
+    	ambulance.play();
+    }
+  }
+});
+
+const girl = document.getElementById('girl');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'u') {
+  	if (!girl.paused) {
+      girl.pause();
+      girl.currentTime = 0;
+    } else {
+    	girl.play();
+    }
+  }
+});
+
+const woo = document.getElementById('woo');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'o') {
+  	if (!woo.paused) {
+      woo.pause();
+      woo.currentTime = 0;
+    } else {
+    	woo.play();
+    }
+  }
+});
+
+const haha = document.getElementById('haha');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'h') {
+  	if (!haha.paused) {
+      haha.pause();
+      haha.currentTime = 0;
+    } else {
+    	haha.play();
+    }
+  }
+});
+
+const loud = document.getElementById('loud');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'x') {
+  	if (!loud.paused) {
+      loud.pause();
+      loud.currentTime = 0;
+    } else {
+    	loud.play();
+    }
+  }
+});
+
+const hear = document.getElementById('hear');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'm') {
+  	if (!hear.paused) {
+      hear.pause();
+      hear.currentTime = 0;
+    } else {
+    	hear.play();
+    }
+  }
+});
+
+const clapping = document.getElementById('clapping');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'c') {
+  	if (!clapping.paused) {
+      clapping.pause();
+      clapping.currentTime = 0;
+    } else {
+    	clapping.play();
+    }
+  }
+});
+
+const jump = document.getElementById('jump');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'v') {
+  	if (!jump.paused) {
+      jump.pause();
+      jump.currentTime = 0;
+    } else {
+    	jump.play();
+    }
+  }
+});
+
+const drink = document.getElementById('drink');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'd') {
+  	if (!drink.paused) {
+      drink.pause();
+      drink.currentTime = 0;
+    } else {
+    	drink.play();
+    }
+  }
+});
+
+const record = document.getElementById('record');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'z') {
+  	if (!record.paused) {
+      record.pause();
+      record.currentTime = 0;
+    } else {
+    	record.play();
+    }
+  }
+});
+
+const omg = document.getElementById('omg');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'k') {
+  	if (!omg.paused) {
+      omg.pause();
+      omg.currentTime = 0;
+    } else {
+    	omg.play();
+    }
+  }
+});
+
+const queen = document.getElementById('queen');
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'n') {
+  	if (!queen.paused) {
+      queen.pause();
+      queen.currentTime = 0;
+    } else {
+    	queen.play();
+    }
+  }
+});
